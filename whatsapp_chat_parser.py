3b0713b1a546664cd7545f9a8d407632565a526d
Raleigh Littles III <raleighlittles@gmail.com>
2024-03-09 23:07:09 -0800

    working on parsing still
    can parse most lines but have to add support for continuation lines

--- whatsapp_chat_parser.py
+++ whatsapp_chat_parser.py
@@ -7,6 +7,21 @@ import pdb
 WHATSAPP_CHAT_FILE_NAME = "_chat.txt"
 
 
+def sanitize_text_line(original_txt_line) -> str:
+    """
+    Not sure why but the WhatsApp messages have weirdly unnecessary Unicode characters in them. For example, the default
+    start message in WhatsApp looks like this in the dump:
+    '[11/5/19, 11:38:00 AM] \u202a\xa0178\xa03464334\u202c: \u200eMessages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.\n'
+    Whereas, when formatted correctly, it should read:
+    [11/5/19, 11:38:00 AM] 17X346XX3X: Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.\n'
+    (the 'XX' is to redact the phone number for privacy)
+    The step below accomplishes that filtering
+    """
+
+    return original_txt_line.encode("ascii", "ignore").decode().strip()
+
+
+
 def get_message_datetime(txt_file_line : str) -> tuple:
     """
     Extract the 'date' the message was sent (in raw form),
@@ -15,21 +30,19 @@ def get_message_datetime(txt_file_line : str) -> tuple:
 
     message_date_regex = re.compile("\[(.*)\]")
 
-    regex_match_obj = message_date_regex.match(txt_file_line)
+    sanitized_txt_file_line = sanitize_text_line(txt_file_line)
+
+    regex_match_obj = message_date_regex.match(sanitized_txt_file_line)
 
     # Will look like: "[11/5/19, 11:38:00 AM]"
     # The regex match captures the enclosing brackets, but we obviously don't want those in the actual time,
     # hence the subtraction
 
-    raw_datetime = txt_file_line[regex_match_obj.span()[0] + 1 : regex_match_obj.span()[1] - 1]
+    raw_datetime = sanitized_txt_file_line[regex_match_obj.span()[0] + 1 : regex_match_obj.span()[1] - 1]
     # https://strftime.org/
     raw_datetime_fmt = "%m/%d/%y, %H:%M:%S %p"
 
     return tuple([datetime.datetime.strptime(raw_datetime, raw_datetime_fmt).strftime("%s"), raw_datetime])
-
-    
-
-
 
     
 def get_message_sender_or_receiver(txt_file_line : str) -> str:
@@ -38,9 +51,11 @@ def get_message_sender_or_receiver(txt_file_line : str) -> str:
     """
     message_phone_num_regex = re.compile("\](.*)\:")
 
-    regex_match_obj = message_phone_num_regex.match(txt_file_line)
 
-    pdb.set_trace()
+    msg_transmitter = re.search(message_phone_num_regex, sanitize_text_line(txt_file_line)).group(1).strip()
+
+    return msg_transmitter
+
 
 
 def convert_whatsapp_chat_to_csv(input_dir : str):
@@ -48,15 +63,32 @@ def convert_whatsapp_chat_to_csv(input_dir : str):
     if os.path.exists(input_dir):
         chat_history_txt_file = os.path.join(input_dir, WHATSAPP_CHAT_FILE_NAME)
 
-        with open(chat_history_txt_file, "rb") as chat_history_file_hndl:
+        with open(chat_history_txt_file, 'r', encoding="utf-8") as chat_history_file_hndl:
+
+            messages = dict()
             
             for line_num, line_contents in enumerate(chat_history_file_hndl.readlines()):
 
-                message = dict()
+                # Line starts with a bracket, it defines the beginning of a new message
+                if line_contents.startswith("["):
 
-                print(f"[DEBUG] Parsing line #{line_num}")
+                    print(f"[DEBUG] Parsing line #{line_num}")
 
-                pdb.set_trace()
+                    msg_datetime = get_message_datetime(line_contents)
+
+                    msg_transmitter = get_message_sender_or_receiver(line_contents)
+
+                    print(f"[DEBUG] Message datetime: {msg_datetime} | Msg transmitter: {msg_transmitter}")
+
+                    messages["time"] = msg_datetime
+                    messages["sender"] = msg_transmitter
+
+                else:
+                    # Line doesn't start with a a bracket, it's just a continuation of the previous message
+
+
+
+
 
 
 
