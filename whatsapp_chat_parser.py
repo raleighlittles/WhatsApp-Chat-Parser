88455f8d402085274d55a0247f54ab4a9d4e3b26
Raleigh Littles III <raleighlittles@gmail.com>
2024-03-20 15:02:00 -0700

    can parse messages correctly
    outputs: '[DEBUG] Finished extracting 6292 messages'

--- whatsapp_chat_parser.py
+++ whatsapp_chat_parser.py
@@ -9,17 +9,22 @@ WHATSAPP_CHAT_FILE_NAME = "_chat.txt"
 
 def sanitize_text_line(original_txt_line) -> str:
     """
-    Not sure why but the WhatsApp messages have weirdly unnecessary Unicode characters in them. For example, the default
+    Some WhatsApp messages include the Unicode left-to-right character markers (U+202A and U+202C) in them. For example, the
     start message in WhatsApp looks like this in the dump:
     '[11/5/19, 11:38:00 AM] \u202a\xa0178\xa03464334\u202c: \u200eMessages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.\n'
     Whereas, when formatted correctly, it should read:
     [11/5/19, 11:38:00 AM] 17X346XX3X: Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.\n'
     (the 'XX' is to redact the phone number for privacy)
-    The step below accomplishes that filtering
+    The step below accomplishes that filtering. 
     """
 
     #return original_txt_line.encode("ascii", "ignore").decode().strip()
-    return original_txt_line
+    forbidden_unicode_chars = ["\u202a", "\u202c", "\u202e", "\xa0"]
+
+    regex_expr = "|".join(forbidden_unicode_chars)
+
+    return re.sub(rf"{regex_expr}", "", original_txt_line.strip())
+
 
 
 
@@ -29,17 +34,9 @@ def get_message_datetime(txt_file_line : str) -> tuple:
     returns it, AND, the Linux epoch timestamp for that date
     """
 
-    message_date_regex = re.compile("\[(.*)\]")
+    # Will look like: "[11/5/19, 11:38:00 AM", need to filter out the brackets
 
-    sanitized_txt_file_line = sanitize_text_line(txt_file_line)
-
-    regex_match_obj = message_date_regex.match(sanitized_txt_file_line)
-
-    # Will look like: "[11/5/19, 11:38:00 AM]"
-    # The regex match captures the enclosing brackets, but we obviously don't want those in the actual time,
-    # hence the subtraction
-
-    raw_datetime = sanitized_txt_file_line[regex_match_obj.span()[0] + 1 : regex_match_obj.span()[1] - 1]
+    raw_datetime = txt_file_line.split("]")[0][1:]
     # https://strftime.org/
     raw_datetime_fmt = "%m/%d/%y, %H:%M:%S %p"
 
@@ -53,7 +50,7 @@ def get_message_sender_or_receiver(txt_file_line : str) -> str:
     message_phone_num_regex = re.compile("\](.*)\:")
 
 
-    msg_transmitter = re.search(message_phone_num_regex, sanitize_text_line(txt_file_line)).group(1).strip()
+    msg_transmitter = str(re.search(message_phone_num_regex, sanitize_text_line(txt_file_line)).group(1).strip())
 
     return msg_transmitter
 
@@ -63,13 +60,9 @@ def get_message_contents(txt_file_line : str) -> str:
 
     message_contents_regex = re.compile("(\]).+(:.+)")
     # 1 to skip the colon included
-    try:
-        msg_contents = re.search(message_contents_regex, sanitize_text_line(txt_file_line))
-    
-    except AttributeError:
-        pdb.set_trace()
-        msg_contents = re.search(message_contents_regex, txt_file_line)
 
+    msg_contents = re.search(message_contents_regex, txt_file_line)
+    
     return msg_contents.group(2)[1:]
 
 
@@ -91,13 +84,13 @@ def convert_whatsapp_chat_to_csv(input_dir : str):
 
                     message = dict()
 
-                    print(f"[DEBUG] Parsing line #{line_num}")
+                    #print(f"[DEBUG] Parsing line #{line_num}")
 
                     msg_datetime = get_message_datetime(line_contents)
                     msg_transmitter = get_message_sender_or_receiver(line_contents)
                     msg_contents = get_message_contents(line_contents)
 
-                    print(f"[DEBUG] Message datetime: '{msg_datetime}' | Msg transmitter: '{msg_transmitter}' | Msg: '{msg_contents}'")
+                    #print(f"[DEBUG] Message datetime: '{msg_datetime}' | Msg transmitter: '{msg_transmitter}' | Msg: '{msg_contents}'")
 
                     message["time"] = msg_datetime
                     message["sender"] = msg_transmitter
@@ -110,7 +103,7 @@ def convert_whatsapp_chat_to_csv(input_dir : str):
                     # Line doesn't start with a a bracket, it's just a continuation of the previous message
                     messages[-1]["contents"] += line_contents
 
-            pdb.set_trace()
+            print(f"[DEBUG] Finished extracting {len(messages)} messages")
 
                 
 
